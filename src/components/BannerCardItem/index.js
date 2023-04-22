5e990b878e3979cd62e608ad9020afb9d21818cb
raviiitejaaa9 <raviiitejaaa9@gmail.com>
2023-04-22 06:33:36 +0000

    final code

--- src/components/BannerCardItem/index.js
+++ src/components/BannerCardItem/index.js
@@ -5,14 +5,14 @@ const BannerCardItem = props => {
   const {cardDetails} = props
   const {headerText, description, className} = cardDetails
   return (
-    <div className={className}>
+    <li className={className}>
       <h1 className="head"> {headerText} </h1>
       <p className="para"> {description} </p>
       <button className="buttons" type="button">
         {' '}
         Show More{' '}
       </button>
-    </div>
+    </li>
   )
 }
 
